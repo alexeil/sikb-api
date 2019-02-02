69f91307183dbf32e730e53a00a26eea8b71071d
tboschat tboschat <thierry.boschat@capgemini.com>
2019-02-02 10:48:52 +0100

    add login test

--- server/src/test/java/com/boschat/sikb/api/identification/UserLoginTest.java
+++ server/src/test/java/com/boschat/sikb/api/identification/UserLoginTest.java
@@ -1,7 +1,6 @@
 package com.boschat.sikb.api.identification;
 
 import com.boschat.sikb.AbstractTest;
-import com.boschat.sikb.PersistenceUtils;
 import com.boschat.sikb.model.Credentials;
 import com.boschat.sikb.model.Session;
 import org.junit.jupiter.api.BeforeEach;
@@ -12,15 +11,18 @@ import javax.ws.rs.core.Response;
 import java.io.IOException;
 
 import static com.boschat.sikb.ApiVersion.V1;
+import static com.boschat.sikb.PersistenceUtils.loadUsers;
+import static com.boschat.sikb.PersistenceUtils.truncateData;
 import static com.boschat.sikb.common.configuration.ResponseCode.CREATED;
+import static com.boschat.sikb.common.configuration.ResponseCode.WRONG_LOGIN_OR_PASSWORD;
 
 @DisplayName(" Login a User ")
 class UserLoginTest extends AbstractTest {
 
     @BeforeEach
     void loadDataSuite() throws IOException {
-        PersistenceUtils.truncateData();
-        PersistenceUtils.loadUsers();
+        truncateData();
+        loadUsers();
     }
 
     @Test
@@ -34,5 +36,25 @@ class UserLoginTest extends AbstractTest {
         checkResponse(response, CREATED);
         Session session = getSession(response);
         checkSession(session);
+    }
+
+    @Test
+    @DisplayName(" unknown login ")
+    void unknownLogin() throws Exception {
+        Credentials credentials = new Credentials();
+        credentials.setLogin("testLogin@kin-ball.fr");
+        credentials.setPassword(USER_DEFAULT_PASSWORD);
+        Response response = userLogin(V1, credentials);
+        checkResponse(response, WRONG_LOGIN_OR_PASSWORD);
+    }
+
+    @Test
+    @DisplayName(" wrong password ")
+    void wrongPassword() throws Exception {
+        Credentials credentials = new Credentials();
+        credentials.setLogin(USER_DEFAULT_EMAIL);
+        credentials.setPassword("WrongPassword");
+        Response response = userLogin(V1, credentials);
+        checkResponse(response, WRONG_LOGIN_OR_PASSWORD);
     }
 }
