8c90263d571a215407185b597497950e1285e1f0
tboschat tboschat <thierry.boschat@capgemini.com>
2019-02-15 17:04:40 +0100

    First try to add SSL stuff

--- server/src/test/java/com/boschat/sikb/StartServer.java
+++ server/src/test/java/com/boschat/sikb/StartServer.java
@@ -1,21 +1,66 @@
 package com.boschat.sikb;
 
+import org.eclipse.jetty.http.HttpVersion;
+import org.eclipse.jetty.server.Connector;
+import org.eclipse.jetty.server.HttpConfiguration;
+import org.eclipse.jetty.server.HttpConnectionFactory;
+import org.eclipse.jetty.server.SecureRequestCustomizer;
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.ServerConnector;
+import org.eclipse.jetty.server.SslConnectionFactory;
 import org.eclipse.jetty.server.handler.HandlerCollection;
+import org.eclipse.jetty.util.ssl.SslContextFactory;
 import org.eclipse.jetty.webapp.WebAppContext;
+
+import java.io.File;
+import java.io.FileNotFoundException;
 
 class StartServer {
 
     public static void main(String[] args) throws Exception {
 
-        Server server = new Server(8080);
+        Server server = new Server();
+
+        HttpConfiguration httpConfig = new HttpConfiguration();
+        httpConfig.setSecureScheme("https");
+        httpConfig.setSecurePort(8443);
+        httpConfig.setOutputBufferSize(32768);
+
+        ServerConnector http = new ServerConnector(server, new HttpConnectionFactory(httpConfig));
+        http.setPort(8080);
+        http.setIdleTimeout(30000);
+
+        File keystoreFile = new File("server/src/main/resources/cert.jks");
+        if (!keystoreFile.exists()) {
+            throw new FileNotFoundException(keystoreFile.getAbsolutePath());
+        }
+        SslContextFactory sslContextFactory = new SslContextFactory();
+        sslContextFactory.setKeyStorePath(keystoreFile.getAbsolutePath());
+        sslContextFactory.setKeyStorePassword("SIKB");
+        sslContextFactory.setKeyManagerPassword("SIKB");
+
+        HttpConfiguration httpsConfig = new HttpConfiguration(httpConfig);
+        SecureRequestCustomizer src = new SecureRequestCustomizer();
+        src.setStsMaxAge(2000);
+        src.setStsIncludeSubDomains(true);
+        httpsConfig.addCustomizer(src);
+
+        ServerConnector https = new ServerConnector(server, new SslConnectionFactory(sslContextFactory, HttpVersion.HTTP_1_1.asString()),
+            new HttpConnectionFactory(httpsConfig));
+        https.setPort(8443);
+        https.setIdleTimeout(500000);
+
+        server.setConnectors(new Connector[] { http, https });
+
         HandlerCollection handlers = new HandlerCollection();
         WebAppContext webApp = new WebAppContext();
         webApp.setResourceBase("server/src/main/webapp");
         webApp.setContextPath("/*");
         webApp.setDefaultsDescriptor("server/src/main/webapp/WEB-INF/web.xml");
         handlers.addHandler(webApp);
+
         server.setHandler(handlers);
         server.start();
+        server.join();
     }
 }
