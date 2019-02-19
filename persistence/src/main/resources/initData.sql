7d800b06f28357f874573b064212642b7bcd4c96
tboschat tboschat <thierry.boschat@capgemini.com>
2019-02-19 19:14:52 +0100

    add default User

--- persistence/src/main/resources/initData.sql
+++ persistence/src/main/resources/initData.sql
@@ -5,6 +5,11 @@ values (2, 'website', 'website', 'website');
 INSERT INTO sikb.application
 values (3, 'test', 'test', 'test');
 
+INSERT INTO sikb.user
+VALUES ('myEmail@kin-ball.fr', 'bGKpbc0W5ILoIyoN8N9Qd3PttwOtqqYKUwInP+CTW37j0+inXEpq8Yh4nDp+oBo477C94ltwdhWIQnUYA2dlqQ',
+        'gy7eniQ5OpIB83ufcBYgpZdgKpS/PQjvGUfcjyp1G5eFIjcF6B52AGS93+lmvnb7CDy3hq0rDQahxKN+/sYcvA', null, null, null, null, null,
+        'Y2RjY2QzMzUtNjRiNy00ZmRiLTkwOGUtZGYxNGYxYzM2ZGRkMjAxOS0wMi0xOVQxNzozODoyMC40ODNa', true, '2018-01-18 13:11:00.000000', '2019-02-19 17:38:20.483000');
+
 INSERT INTO sikb.season
 values ('20172018', 'Saison 2017/2018', '2017-09-01', '2018-08-31');
 INSERT INTO sikb.season
